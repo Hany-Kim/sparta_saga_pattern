7ccf4ed5fbaeec405f13bfb267423004dea2aee9
Hany-Kim <hanykim1210@gmail.com>
2025-01-02 02:06:04 +0900

    market.payment.queue -> market.payment.err.queue

--- payment/src/main/java/com/market/payment/PaymentService.java
+++ payment/src/main/java/com/market/payment/PaymentService.java
@@ -3,12 +3,19 @@ package com.market.payment;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Value("${message.queue.err.product}")
+    private String productErrorQueue;
 
     public void createPayment(DeliveryMessage deliveryMessage) {
 
@@ -23,6 +30,13 @@ public class PaymentService {
 
         if(payAmount >= 10000) {
             log.error("Payment amount exceeds limit : {}", payAmount);
+            deliveryMessage.setErrorType("PAYMENT_LIMIT_EXCEED");
+            this.rollbackPayment(deliveryMessage);
         }
+    }
+
+    public void rollbackPayment(DeliveryMessage deliveryMessage) {
+        log.info("PAYMENT ROLLBACK !!!");
+        rabbitTemplate.convertAndSend(productErrorQueue, deliveryMessage);
     }
 }
